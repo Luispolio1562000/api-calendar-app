61ece28409cad8517e33e7a7b155636e7513da7c
Luispolio1562000 <guspolio@outlook.es>
2025-04-30 00:07:59 -0600

    cors

--- index.js
+++ index.js
@@ -23,13 +23,16 @@ const path = require("path");
 const app = express();
 //? BD connections
 dbConnection();
-app.use(cors());
- app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-   next();
- });
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 //? Directorio publico.
 //? Use es un middleware que se ejecuta antes de las rutas.
